433f3aa4e237010ab7e32386bafdd3481cc49be8
kamontat <kamontat_c@hotmail.com>
2017-07-16 22:18:50 +0700

    Final tutorial AngularJS.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,38 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }   from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroSearchComponent } from './hero-search.component';
+
+import { HeroService } from './hero.service';
+
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    HeroSearchComponent,
+    DashboardComponent
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
